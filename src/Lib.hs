b7821ff13913bc98a6f7b5bddb56726f545a2c77
Unknown <christopher.nyberg@gmail.com>
2017-03-18 17:45:38 -0700

    Nailed it.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,15 +2,53 @@ module Lib
     ( grid
     , languages
     , outputGrid
+    , solveGrid
     ) where
 
+import Data.List (isInfixOf, transpose)
+import Data.Maybe (catMaybes)
 
-outputGrid :: [String] -> IO ()
+type Grid = [String]
+
+outputGrid :: Grid -> IO ()
 outputGrid = putStrLn . formatGrid
 
-formatGrid :: [String] -> String
+formatGrid :: Grid -> String
 formatGrid = unlines 
 
+gridBothWays :: Grid -> Grid
+gridBothWays grid' = grid' ++ fmap reverse grid'
+
+getLines :: Grid -> [String]
+getLines grid' = horiVertDiag
+    where horiVertDiag = hori ++ vert ++ sE ++ nE
+          hori = gridBothWays grid'
+          vert = gridBothWays $ transpose grid'
+          sE = gridBothWays $ transpose $ skew grid'
+          nE = gridBothWays $ transpose $ skew $ reverse grid'
+
+findWord :: Grid -> String -> Maybe String
+findWord grid' word = if found then Just word else Nothing
+    where found = or $ findWordInLine word <$> getLines grid'
+
+findWords :: Grid -> [String] -> [String]
+findWords grid' words' = catMaybes foundWords
+    where foundWords = fmap (findWord grid') words'
+
+findWordInLine :: String -> String -> Bool
+findWordInLine = isInfixOf
+
+skew :: Grid -> Grid
+skew [] = []
+skew (l:ls) = l : skew (fmap indent ls)
+    where indent line = '_' : line
+
+solveGrid :: [String]
+solveGrid = findWords grid languages
+
+
+
+grid :: Grid
 grid = [ "__C________R___"
        , "__SI________U__"
        , "__HASKELL____B_"
@@ -25,6 +63,7 @@ grid = [ "__C________R___"
        , "________CN_____"
        ]
 
+languages :: [String]
 languages = [ "BASIC"
             , "COBOL"
             , "CSHARP"
